ef3daf09c87cd8de0d40ad2a61ee6c425f4f41e8
yoshinbo <yoshinbo@gmail.com>
2015-10-28 20:01:51 +0900

    Initial Commit

---
+++ aromaTests/aromaTests.swift
@@ -0,0 +1,36 @@
+//
+//  aromaTests.swift
+//  aromaTests
+//
+//  Created by Yoshikazu Oda on 2015/10/28.
+//  Copyright © 2015年 Yoshikazu Oda. All rights reserved.
+//
+
+import XCTest
+@testable import aroma
+
+class aromaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
