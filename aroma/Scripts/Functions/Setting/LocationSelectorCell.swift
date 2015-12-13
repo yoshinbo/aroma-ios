29951ca20f0d88890deb0352e12779af0532807c
yoshinbo <yoshinbo@gmail.com>
2015-12-13 16:53:15 +0900

    都道府県を選択できるように修正

---
+++ aroma/Scripts/Functions/Setting/LocationSelectorCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationSelectorCell.swift
+//  aroma
+//
+//  Created by Yoshikazu Oda on 2015/12/13.
+//  Copyright © 2015年 Yoshikazu Oda. All rights reserved.
+//
+
+import UIKit
+
+class LocationSelectorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ aroma/Scripts/Functions/Setting/LocationSelectorDataHandler.swift
@@ -0,0 +1,9 @@
+//
+//  LocationSelectorDataHandler.swift
+//  aroma
+//
+//  Created by Yoshikazu Oda on 2015/12/13.
+//  Copyright © 2015年 Yoshikazu Oda. All rights reserved.
+//
+
+import Foundation
